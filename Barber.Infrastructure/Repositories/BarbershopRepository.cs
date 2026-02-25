7bdbd1499584edff4c5e160fc2c9a1f74391bef5
Flávio Eduardo <xflax00@gmail.com>
2026-02-25 20:59:58 -0300

    feat: add barbershop repository

---
+++ Barber.Infrastructure/Repositories/BarbershopRepository.cs
@@ -0,0 +1,13 @@
+﻿using Barber.Domain.Entities;
+using Barber.Domain.Interfaces.Repositories;
+using Barber.Infrastructure.Contexts;
+using Barber.Infrastructure.Repositories.Base;
+
+namespace Barber.Infrastructure.Repositories;
+
+public class BarbershopRepository : BaseRepository<Barbershop>, IBarbershopRepository
+{
+    public BarbershopRepository(BarberDbContext context) : base(context)
+    {
+    }
+}
